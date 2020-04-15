9081dc54b20f4cb744553b0380076f5c67a790ff
melcbos <53103433+melcbos@users.noreply.github.com>
2020-04-15 16:27:21 -0400

    Added files

---
+++ courseList.py
@@ -0,0 +1,5 @@
+# Make sure to fill in your courses below in FORMAT School + Course ID
+# Examples: "CASCS320 A1", "QSTAC222 B1"
+# Term examples: "2020FALL", "2020SPR"
+targets = ["CASCS320 A1", "QSTAC222 B1"]
+term = "2020FALL"
