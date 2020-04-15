4e6f93d563265a621b983680612bf42ac069300b
melcbos <53103433+melcbos@users.noreply.github.com>
2020-04-15 17:38:08 -0400

    Delete courseList.py

--- courseList.py
+++
@@ -1,5 +0,0 @@
-# Make sure to fill in your courses below in FORMAT School + Course ID
-# Examples: "CASCS320 A1", "QSTAC222 B1"
-# Term examples: "2020FALL", "2020SPR"
-targets = ["CASCS320 A1", "QSTAC222 B1"]
-term = "2020FALL"
